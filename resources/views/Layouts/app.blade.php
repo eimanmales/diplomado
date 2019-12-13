bb3629bd0c9aab29aea2eb30bf5eb3926f20e962
males <maleseiman@gmail.com>
2019-12-12 20:54:18 -0500

    vavbar

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -11,9 +11,31 @@
 	
 </head>
 <body>
-	<nav class="navbar navbar-dark bg-primary">
+	<!--<nav class="navbar navbar-dark bg-primary">
 		<a href="#" class="navbar-brand">Person@s</a>
-	</nav>
+	</nav>-->
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			<a href="#" class="navbar-brand">Person@s|Diplomado</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			  <span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+			  <ul class="navbar-nav">
+				<li class="nav-item active">
+				  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="{{route('comuna.index')}}">Comuna</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="{{route('municipio.index')}}">Municipio</a>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+				</li>
+			  </ul>
+			</div>
+		  </nav>
 
 	<div class="container">
 		<div style="background-color:#333; color:#9d9d9d; margin-top:5px">
