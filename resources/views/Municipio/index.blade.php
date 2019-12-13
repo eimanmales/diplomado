3c79a8acea69d75c2f8eeee2166fbcd73ba0b2a9
males <maleseiman@gmail.com>
2019-12-12 21:14:53 -0500

    municipio

--- resources/views/Municipio/index.blade.php
+++ resources/views/Municipio/index.blade.php
@@ -1,23 +1,23 @@
 
 @extends('layouts.app')
 
-@section('title', 'Listado de Comunas')
+@section('title', 'Listado de Municipio')
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.28.2/sweetalert2.all.js"></script>
 
-@section('title2', 'Listado de Comunas')
+@section('title2', 'Listado de Municipio')
 
 @section('content')
 	<table id="tabla" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%" style="margin-top:10px">
         <thead>
             <tr>
                 <th>Codigo</th>
-                <th>Comuna</th>            
-                <th>Municipio</th>
+                <th>Municipio</th>            
+                <th>Departamento</th>
                 <th class="text-center">
-                    <a href="/comuna/create" class="btn btn-primary btn-sm" id="nuevo"  
-                        data-toggle="tooltip" title="Nueva Comuna">
+                    <a href="/municipio/create" class="btn btn-primary btn-sm" id="nuevo"  
+                        data-toggle="tooltip" title="Nuevo Municipio">
                         <i class="fa fa-plus" aria-hidden="true"></i>
                         Nueva
                     </a> 
@@ -27,19 +27,19 @@
         <tbody>
             @include('common.success')
 
-            @foreach($comunas as $comuna)
+            @foreach($municipios as $municipio)
                 <tr>
-                    <td>{{$comuna->comu_codi}}</td>
-                    <td>{{$comuna->comu_nomb}}</td>
-                    <td>{{$comuna->muni_nomb}}</td>
+                    <td>{{$municipio->muni_codi}}</td>
+                    <td>{{$municipio->muni_nomb}}</td>
+                    <td>{{$municipio->depa_nomb}}</td>
                     <td class="text-center">
-                        <form method="POST" action="/comuna/{{$comuna->comu_codi}}" accept-charset="UTF-8" 
+                        <form method="POST" action="/municipio/{{$municipio->muni_codi}}" accept-charset="UTF-8" 
                             style="display:inline">
                             @csrf
                             <input name="_method" type="hidden" value="DELETE">					
                             <button type="submit" class="btn btn-danger btn-sm fa fa-trash" style="margin-right: 10px">	</button>				
                         </form>
-                        <a href="/comuna/{{$comuna->comu_codi}}/edit"><i class="btn btn-info btn-sm fa fa-edit"></i></a>
+                        <a href="/municipio/{{$municipio->muni_codi}}/edit"><i class="btn btn-info btn-sm fa fa-edit"></i></a>
                     </td>
                 </tr>
             @endforeach
